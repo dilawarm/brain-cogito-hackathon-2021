db21ef872a0a977c9383f09fb53f0a71867e0643
Dilawar Mahmood <dilawarmm@outlook.com>
2021-03-13 20:15:50 +0100

    finish std?

--- preprocessing.py
+++ preprocessing.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 
-def load_data(path='hackathon_kpis_anonymised.csv'):
+def load_data(path='ml_models_dataset_with_zone.csv'):
     print('Load', path)
     kpis = pd.read_csv(path)
     kpis['timestamp'] = pd.to_datetime(kpis['timestamp'])
     kpis.sort_values('timestamp', inplace=True)
     n = len(kpis)
-    idx_train = int(n*.7)
-    idx_val = idx_train + int(n*.15)
+    idx_train = int(n)
+    idx_val = idx_train
     train = kpis[:idx_train]
-    val = kpis[idx_train:idx_val]
-    test = kpis[idx_val:]
-    print(train.shape, val.shape, test.shape)
-    return train, val, test
+    #val = kpis[idx_train:idx_val]
+    #test = kpis[idx_val:]
+    #print(train.shape, val.shape, test.shape)
+    return train
 
 
 if __name__ == '__main__':
