49bc9fd4b4648a3b8972ff9f556eae0ce3ca980a
Simen Burud <sb@simen.codes>
2021-03-13 19:51:15 +0100

    Train DeepAnT on individual cell towers and new data

--- DeepAnT.py
+++ DeepAnT.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 import torch
 from sklearn.model_selection import train_test_split
-from tqdm.auto import trange
+from tqdm.auto import trange, tqdm
 
-from DeepAnT_model import DeepAnT
+from DeepAnT_model import DeepAnT, DeepAnT2d
 
 EPOCHS = 10
 SAMPLE_STEPS = 10
@@ -22,29 +22,11 @@ def make_samples(df):
     return X, Y
 
 
-df = pd.read_csv("hackathon_kpis_anonymised/ml_models_dataset.csv")
+df = pd.read_csv("hackathon_kpis_anonymised/ml_models_dataset_with_zone.csv")
+
+
 # df['timestamp'] = pd.to_datetime(df['timestamp'])
-df.drop(columns='timestamp', inplace=True)
-
-X, Y = make_samples(df)
-
-X, Y = map(lambda x: torch.tensor(x, dtype=torch.float32), (X, Y))
-
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.25, random_state=42, shuffle=False)
-
-model = DeepAnT(SAMPLE_STEPS, len(df.columns)).to('cuda')
-
-try:
-    model.load_state_dict(torch.load('models/epoch_8__eval_loss_0.0008937545935623348.pth'))
-except Exception as e:
-    print(e)
-    print('Can\'t load state dict, training model')
-    for epoch in trange(EPOCHS):
-        train_loss = model.train_epoch(X_train, Y_train)
-        eval_loss = model.evaluate(X_test, Y_test)
-        print('Epoch {0} done\t train_loss={1} \t eval_loss={2}'.format(epoch, train_loss, eval_loss))
-        torch.save(model.state_dict(), 'models/epoch_{0}__eval_loss_{1}.pth'.format(epoch, eval_loss))
+# df.drop(columns='timestamp', inplace=True)
 
 
 def get_anomaly_score(model, data, true_next_value):
@@ -53,11 +35,29 @@ def get_anomaly_score(model, data, true_next_value):
     return torch.norm(pred_next_value - true_next_value.to(model.device), dim=1).cpu().detach().numpy()
 
 
-anom_score = get_anomaly_score(
-    model,
-    torch.from_numpy(X.astype(np.float32)),
-    torch.from_numpy(Y.astype(np.float32))
-)
-plt.plot(df.index[10:], anom_score)
-plt.title('Anomaly scores on FULL dataset')
-plt.show()
+def model_for_dataframe(name, df):
+    X, Y = make_samples(df.drop(columns=['timestamp', 'cell_name']))
+    X, Y = map(lambda x: torch.tensor(x, dtype=torch.float32), (X, Y))
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.25, random_state=42, shuffle=False)
+
+    model = DeepAnT2d(SAMPLE_STEPS, X_train.shape[-1]).to('cuda')
+    for epoch in trange(EPOCHS):
+        train_loss = model.train_epoch(X_train, Y_train, verbose=name == 'all')
+        eval_loss = model.evaluate(X_test, Y_test)
+        if name == 'all':
+            print('Epoch {0} done\t train_loss={1} \t eval_loss={2}'.format(epoch, train_loss, eval_loss))
+        torch.save(model.state_dict(), 'models/deepant_{0}_epoch_{1}__eval_loss_{2}.pth'.format(name, epoch, eval_loss))
+
+    anom_score = get_anomaly_score(model, X, Y)
+    plt.plot(df.index[10:], anom_score)
+    plt.title('Anomaly scores from ' + name)
+    plt.savefig('figs/deepant_' + name + '.png')
+    plt.close()
+    return model
+
+
+for name, idx in tqdm(df.groupby('cell_name').groups.items()):
+    model_for_dataframe(name, df.loc[idx])
+model_for_dataframe('all', df)
--- DeepAnT_model.py
+++ DeepAnT_model.py
@@ -9,6 +9,40 @@ class DeepAnT(torch.nn.Module):
 
     def __init__(self, time_steps, n_features):
         super(DeepAnT, self).__init__()
+        self.criterion = torch.nn.MSELoss(reduction='mean')
+        self.optimizer = torch.optim.Adam(list(self.parameters()), lr=1e-5)
+
+    @property
+    def device(self):
+        return self.conv1d_1_layer.bias.get_device()
+
+    def train_epoch(self, X_train: torch.Tensor, Y_train: torch.Tensor, verbose=True):
+        train_data = torch.utils.data.TensorDataset(X_train, Y_train)
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_data, batch_size=32, shuffle=True)
+        self.train()
+        loss_sum = 0.0
+        steps = 0
+        for x, y in tqdm(train_loader) if verbose else train_loader:
+            x, y = x.to(self.device), y.to(self.device)
+            yhat = self(x)
+            loss = self.criterion(y, yhat)
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            loss_sum += loss.item()
+            steps += 1
+        return loss_sum / steps
+
+    def evaluate(self, X, Y):
+        self.eval()
+        yhat = self(X)
+        return self.criterion(yhat, Y.to(self.device)).item()
+
+
+class DeepAnT2d(DeepAnT):
+    def __init__(self, time_steps, n_features):
+        super().__init__(time_steps, n_features)
         self.conv1d_1_layer = torch.nn.Conv1d(
             in_channels=time_steps, out_channels=16, kernel_size=3)
         self.relu_1_layer = torch.nn.ReLU()
@@ -18,17 +52,10 @@ class DeepAnT(torch.nn.Module):
         self.relu_2_layer = torch.nn.ReLU()
         self.maxpooling_2_layer = torch.nn.MaxPool1d(kernel_size=2)
         self.flatten_layer = torch.nn.Flatten()
-        self.dense_1_layer = torch.nn.Linear(80, n_features * 2)
+        self.dense_1_layer = torch.nn.Linear(96, n_features * 2)
         self.relu_3_layer = torch.nn.ReLU()
         self.dropout_layer = torch.nn.Dropout(p=0.25)
         self.dense_2_layer = torch.nn.Linear(n_features * 2, n_features)
-
-        self.criterion = torch.nn.MSELoss(reduction='mean')
-        self.optimizer = torch.optim.Adam(list(self.parameters()), lr=1e-5)
-
-    @property
-    def device(self):
-        return self.conv1d_1_layer.bias.get_device()
 
     def forward(self, x):
         x = x.to(self.device)
@@ -42,33 +69,45 @@ class DeepAnT(torch.nn.Module):
         try:
             x = self.dense_1_layer(x)
         except RuntimeError as e:
-            print(x.shape)
+            print('Shape error, please update according to the dataset you\'re using', x.shape)
             raise e
         x = self.relu_3_layer(x)
         x = self.dropout_layer(x)
         return self.dense_2_layer(x)
 
-    def train_epoch(self, X_train: torch.Tensor, Y_train: torch.Tensor):
-        train_data = torch.utils.data.TensorDataset(X_train, Y_train)
-        train_loader = torch.utils.data.DataLoader(
-            dataset=train_data, batch_size=32, shuffle=True)
-        self.train()
-        loss_sum = 0.0
-        steps = 0
-        for x, y in tqdm(train_loader):
-            x, y = x.to(self.device), y.to(self.device)
-            yhat = self(x)
-            loss = self.criterion(y, yhat)
-            loss.backward()
-            self.optimizer.step()
-            self.optimizer.zero_grad()
-            loss_sum += loss.item()
-            steps += 1
-            if steps % 10_000 == 0:
-                print('Train loss: {0} step {1}'.format(float(loss_sum / steps), steps))
-        return loss_sum / steps
 
-    def evaluate(self, X, Y):
-        self.eval()
-        yhat = self(X)
-        return self.criterion(yhat, Y.to(self.device)).item()
+class DeepAnT3d(DeepAnT):
+    def __init__(self, time_steps, n_features):
+        super().__init__(time_steps, n_features)
+        self.conv1d_1_layer = torch.nn.Conv1d(
+            in_channels=time_steps, out_channels=16, kernel_size=3)
+        self.relu_1_layer = torch.nn.ReLU()
+        self.maxpooling_1_layer = torch.nn.MaxPool1d(kernel_size=2)
+        self.conv1d_2_layer = torch.nn.Conv1d(
+            in_channels=16, out_channels=16, kernel_size=3)
+        self.relu_2_layer = torch.nn.ReLU()
+        self.maxpooling_2_layer = torch.nn.MaxPool1d(kernel_size=2)
+        self.flatten_layer = torch.nn.Flatten()
+        self.dense_1_layer = torch.nn.Linear(96, n_features * 2)
+        self.relu_3_layer = torch.nn.ReLU()
+        self.dropout_layer = torch.nn.Dropout(p=0.25)
+        self.dense_2_layer = torch.nn.Linear(n_features * 2, n_features)
+        raise NotImplementedError()
+
+    def forward(self, x):
+        x = x.to(self.device)
+        x = self.conv1d_1_layer(x)
+        x = self.relu_1_layer(x)
+        x = self.maxpooling_1_layer(x)
+        x = self.conv1d_2_layer(x)
+        x = self.relu_2_layer(x)
+        x = self.maxpooling_2_layer(x)
+        x = self.flatten_layer(x)
+        try:
+            x = self.dense_1_layer(x)
+        except RuntimeError as e:
+            print('Shape error, please update according to the dataset you\'re using', x.shape)
+            raise e
+        x = self.relu_3_layer(x)
+        x = self.dropout_layer(x)
+        return self.dense_2_layer(x)
